6b10adf635803e3a96b69a23d5edf8510f096eb7
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-03-23 19:03:30 -0500

    Add files via upload

---
+++ Quiz_2_EvenOdd.cpp
@@ -0,0 +1,20 @@
+/*
+Quiz #2 - Even or Odd
+Name
+3/26/2021
+Using the comments complete the program and resubmit.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "I can tell you whether a number is even or odd.\n";
+    cout << "Please enter a number >> ";
+    cin >> number;
+    // Write an if statement to determine and report whether the entered number
+    // is even or odd.
+    return 0;
+}
